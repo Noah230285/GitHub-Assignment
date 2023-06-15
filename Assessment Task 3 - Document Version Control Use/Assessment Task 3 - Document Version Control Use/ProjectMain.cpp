ddd75ed8ebd5e7a73b826af7fe2f86902a5caa2d
Noah230285 <noahjharris@outlook.com>
2023-06-15 16:36:56 +1000

    Added user input
    User can type into the console and the program will receive that string.
    Additionally, program now uses namespace std.

--- Assessment Task 3 - Document Version Control Use/Assessment Task 3 - Document Version Control Use/ProjectMain.cpp
+++ Assessment Task 3 - Document Version Control Use/Assessment Task 3 - Document Version Control Use/ProjectMain.cpp
@@ -1,9 +1,19 @@
 #include "ProjectMain.h"
 
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Hello World" << std::endl;
+	cout << "Hello World" << endl;
+
+	cout << "Please enter name" << endl;
+
+	string input = "";
+	cin >> input;
+
+	cout << "Hello " << input.c_str() << endl;
+
 	return 0;
 }
